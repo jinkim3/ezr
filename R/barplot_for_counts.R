9fcc87a34b9352a4fecad027ca580dd620ae0a80
jinkim3 <jin.m.kim@yale.edu>
2020-11-17 19:53:29 -0500

    initial

---
+++ R/barplot_for_counts.R
@@ -0,0 +1,45 @@
+#' Barplot for counts
+#'
+#' @param data a data.frame or data.table object
+#' @param x name of the variable that will be on the x axis of the barplot
+#' @param y name of the variable that will be on the y axis of the barplot
+#' @examples
+#' barplot_for_counts(
+#' data = data.frame(cyl = names(table(mtcars$cyl)),
+#' count = as.vector(table(mtcars$cyl))),
+#' x = "cyl", y = "count")
+#' @export
+#' @import ggplot2
+barplot_for_counts <- function(data = NULL, x, y) {
+  data <- data.table::setDT(data)
+  # check for x axis
+  if (!is.null(data) & x %in% names(data) & y %in% names(data)) {
+    dt <- data[, c(x, y), with = FALSE]
+    g1 <- ggplot(dt, aes(x = get(x), y = get(y)))
+    g1 <- g1 + xlab(x)
+    g1 <- g1 + ylab(y)
+  }
+  # set up data table
+  if (is.null(data)) {
+    dt <- data.table::data.table(x = x, y = y)
+    g1 <- ggplot(dt, aes(x = x, y = y))
+  }
+  g1 <- g1 + geom_bar(stat = "identity")
+  # plot theme
+  g1 <- g1 + theme_classic(base_size = 20) %+replace%
+    theme(
+      plot.title = element_text(hjust = 0.5),
+      axis.title.x = element_text(margin = margin(t = 24)),
+      axis.title.y = element_text(
+        angle = 0, vjust = 0.85,
+        margin = margin(r = 24)),
+      axis.title = element_text(
+        face = "bold", color = "black", size = 24),
+      axis.text = element_text(
+        face = "bold", color = "black", size = 20),
+      legend.title = element_text(
+        face = "bold", color = "black", size = 24),
+      legend.text = element_text(
+        face = "bold", color = "black", size = 20))
+  return(g1)
+}
---
+++ README.md
@@ -0,0 +1,53 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# ezr
+
+<!-- badges: start -->
+
+[![Travis build
+status](https://travis-ci.com/jinkim3/ezr.svg?branch=master)](https://travis-ci.com/jinkim3/ezr)
+[![R build
+status](https://github.com/jinkim3/ezr/workflows/R-CMD-check/badge.svg)](https://github.com/jinkim3/ezr/actions)
+<!-- badges: end -->
+
+Point and click with your mouse to conduct basic analyses and create
+graphics quickly and easily. This package will run a “Shiny App” on your
+local machine, which allows you to analyze data without typing a line of
+code. You do not need to know anything about R to use this tool.
+
+Examples of things you can do include (but are not limited to):
+
+  - getting descriptive statistics by experimental group
+  - creating histograms by experimental group
+  - creating scatter plots
+  - calculating correlations
+  - running multiple regressions
+
+## Installation
+
+You can install the released version of kim from
+[CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("ezr")
+```
+
+You can also install the development version from
+[GitHub](https://github.com/) with:
+
+``` r
+install.packages("devtools")
+devtools::install_github("jinkim3/ezr")
+```
+
+## Example
+
+Here are some examples of using this package.
+
+``` r
+library(ezr)
+
+# Start the Shiny server on a local machine
+ezr::start_shiny()
+```
